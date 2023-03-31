5bf38b858b432acf49c60bf13ff2927f2330359f
Rayhanyoshh <Rayhan.yoshara@gmail.com>
2023-03-31 17:48:15 +0700

    Add project files.

---
+++ TranScopeCommon/ITranScope.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TranScopeCommon
+{
+    public interface ITranScope
+    {
+        TranScopeResultDTO ProcessWithoutTransaction(int ProcessRecordCount);
+        TranScopeResultDTO ProcessAllWithTransactionDB(int poProcessRecordCount);
+    }
+}
---
+++ TranScopeService/TranScopeController.cs
@@ -0,0 +1,61 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using R_Common;
+using TranScopeBack;
+using TranScopeCommon;
+
+
+namespace TransScopeService
+{
+
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TranScopeController : ControllerBase, ITranScope
+    {
+        [HttpPost]
+        public TranScopeResultDTO ProcessAllWithTransactionDB(int poProcessRecordCount)
+        {
+            R_Exception loException = new R_Exception();
+            TranScopeResultDTO loRtn = null;
+            TranScopeCls loCls;
+
+            try
+            {
+                loCls = new TranScopeCls();
+                loRtn = new TranScopeResultDTO();
+                loRtn.data = loCls.ProcessAllWithTransactionDB(poProcessRecordCount);
+            }
+            catch (Exception ex)
+            {
+                loException.Add(ex);
+            }
+        EndBlock:
+            loException.ThrowExceptionIfErrors();
+
+            return loRtn;
+        }
+
+        [HttpPost]
+        public TranScopeResultDTO ProcessWithoutTransaction(int ProcessRecordCount)
+        {
+            R_Exception loException = new R_Exception();
+            TranScopeResultDTO loRtn = null;
+            TranScopeCls loCls;
+
+            try
+            {
+                loCls = new TranScopeCls();
+                loRtn = new TranScopeResultDTO();
+                loRtn.data = loCls.ProcessWithoutTransactionDB(ProcessRecordCount);
+            }
+            catch (Exception ex)
+            {
+                loException.Add(ex);
+            }
+        EndBlock:
+            loException.ThrowExceptionIfErrors();
+
+            return loRtn;
+        }
+    }
+}
